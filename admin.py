81a0664085a31f8d3aa578ed1f554fd9df233cbd
himanshi-ratech <145336411+himanshi-ratech@users.noreply.github.com>
2025-10-31 11:49:35 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Register, Student, Faculty, Query, userprofile, Event, Registration, Post, Comment, Student_Detail
+
+admin.site.register(Register)
+admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(Query)
+admin.site.register(userprofile)
+admin.site.register(Event)
+admin.site.register(Registration)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Student_Detail)
+
+
+# Register your models here.
